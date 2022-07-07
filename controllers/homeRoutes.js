f111596ad53ad3cd7dfec0fbeefa661c5c3c9d63
Jean Lee <jeanlee.jingli@icloud.com>
2022-07-07 14:34:56 -0700

    add some mpdifies

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,80 @@
+const router = require('express').Router();
+const { User, Blog, Comment } = require('../models');
+
+// GET all Blogs for homepage
+router.get('/', async (req, res) => {
+  try {
+    const dbBlogData = await Blog.findAll({
+      include: [
+        {
+          model: User,
+        },
+      ],
+    });
+
+    const blogs = dbBlogData.map((blog) =>
+    blog.get({ plain: true })
+    );
+
+    res.render('homepage', {
+      blogs,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one gallery
+router.get('/gallery/:id', async (req, res) => {
+  try {
+    const dbGalleryData = await Gallery.findByPk(req.params.id, {
+      include: [
+        {
+          model: Painting,
+          attributes: [
+            'id',
+            'title',
+            'artist',
+            'exhibition_date',
+            'filename',
+            'description',
+          ],
+        },
+      ],
+    });
+
+    const gallery = dbGalleryData.get({ plain: true });
+    res.render('gallery', {
+      gallery,
+      // We are not incrementing the 'countVisit' session variable here
+      // but simply sending over the current 'countVisit' session variable to be rendered
+      countVisit: req.session.countVisit,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET one painting
+router.get('/painting/:id', async (req, res) => {
+  try {
+    const dbPaintingData = await Painting.findByPk(req.params.id);
+
+    const painting = dbPaintingData.get({ plain: true });
+
+    res.render('painting', {
+      painting,
+      // We are not incrementing the 'countVisit' session variable here
+      // but simply sending over the current 'countVisit' session variable to be rendered
+      countVisit: req.session.countVisit,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
