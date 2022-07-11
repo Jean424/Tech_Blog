c4ae840d60fbd98755418c43ac21039c319afc3a
Jean Lee <jeanlee.jingli@icloud.com>
2022-07-10 23:35:47 -0700

    modify controller index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,7 +4,7 @@ const blogRoutes = require('./blogRoutes');
 const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
-router.use('/blogs', projectRoutes);
-router.use('/comments', projectRoutes);
+router.use('/blogs', blogRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
