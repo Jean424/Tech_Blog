28369a6cf515e8c9d059dafcce29cbff2d4b3e10
Jean Lee <jeanlee.jingli@icloud.com>
2022-07-11 19:14:43 -0700

    made some modifiies in the project

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,13 +1,13 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#blog-title').value.trim();
-  const content = document.querySelector('#project-cont').value.trim();
+  const title = document.querySelector('#blog-title').value.trim();
+  const contents = document.querySelector('#blog-cont').value.trim();
 
-  if (title && content) {
+  if (title && contents) {
     const response = await fetch(`/api/blogs`, {
       method: 'POST',
-      body: JSON.stringify({ title, content }),
+      body: JSON.stringify({ title, contents }),
       headers: {
         'Content-Type': 'application/json',
       },
