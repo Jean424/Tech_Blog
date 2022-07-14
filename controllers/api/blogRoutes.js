fb105ab361dbd0cf35cacd2b2cf56c3a325c9731
Jean Lee <jeanlee.jingli@icloud.com>
2022-07-13 22:55:25 -0700

    modify the blogRoutes.js

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -3,36 +3,78 @@ const { Blog } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
-  try {
-    const newBlog = await Blog.create({
-      ...req.body,
-      user_id: req.session.user.id,
-    });
-
-    res.status(200).json(newBlog);
-  } catch (err) {
-    res.status(400).json(err);
+  if (!req.session.user) {
+    return res.status(401).json({ msg: "You need to login to post a Blog!" })
   }
+  Blog.create({
+    name: req.body.name,
+    body: req.body.body,
+    user_id: req.session.user.id
+  })
+    .then(newBlog => {
+      res.json(newBlog);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ msg: "an error occured", err });
+    });
 });
 
-router.delete('/:id', withAuth, async (req, res) => {
-  try {
-    const deleteData = await Blog.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user.id,
-      },
+//gets blogs
+router.get("/", (req, res) => {
+  Blog.findAll({
+    include: { all: true, nested: true }
+  })
+    .then(dbBlogs => {
+      res.json(dbBlogs);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ msg: "an error occured", err });
     });
+});
 
-    if (!deleteData) {
-      res.status(404).json({ message: 'No blog found with this id!' });
-      return;
+//single blog grab
+router.get("/:id", (req, res) => {
+  Blog.findByPk(req.params.id, {})
+    .then(dbBlogs => {
+      res.json(dbBlogs);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ msg: "an error occured", err });
+    });
+});
+
+//UPDATE
+router.put("/:id", (req, res) => {
+  Blog.update(req.body, {
+    where: {
+      id: req.params.id
     }
+  }).then(updatedBlog => {
+    res.json(updatedBlog);
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ msg: "an error occured", err });
+    });
+});
 
-    res.status(200).json(deleteData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+//DELETE
+router.delete('/:id', withAuth, async (req, res) => {
+  Blog.destroy({
+    where: {
+      id: req.params.id,
+      user_id: req.session.user.id
+    }
+  }).then(delBlog => {
+    res.json(delBlog)
+    location.reload();
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({ msg: "an error occured", err });
+  })
 });
 
 module.exports = router;
